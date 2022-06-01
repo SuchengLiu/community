c6e651ec667f63c9b908d2ccc571385d06f49c2e
SuchengLiu <992863223@qq.com>
2022-06-01 19:19:15 +0800

    开发了社区登录模块 登录页面找回密码功能在获取验证码，无法给服务器传入填写的邮箱 个人设置页面的重置密码功能交互性不友好

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,33 @@
+package com.nowcoder.community.controller.interceptor;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 判断成功后，直接将handler转型为HandlerMethod并实例化为handlerMethod
+        if (handler instanceof HandlerMethod handlerMethod) {
+            Method method = handlerMethod.getMethod();
+            LoginRequired loginRequired = method.getAnnotation(LoginRequired.class);
+            if (loginRequired != null && hostHolder.getUser() == null) {
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
+        }
+        return true;
+    }
+}
